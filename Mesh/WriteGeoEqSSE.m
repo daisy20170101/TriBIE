ade386ad3246a83f0c37e0713ffa053045bc3d3f
DL <liduoduo07@gmail.com>
2022-07-04 09:44:28 +0200

    update

--- Mesh/WriteGeoEqSSE.m
+++ Mesh/WriteGeoEqSSE.m
@@ -1,3 +1,39 @@
+% @file
+% This file is part of TriBIE.
+%
+% @author Duo Li dli@geophysik.uni-muenchen.de 
+%
+% @section LICENSE
+% Copyright (c) 2022, TriBIE Group
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its
+%    contributors may be used to endorse or promote products derived from this
+%    software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+% @section DESCRIPTION
 %% write geometry, unit is meter.
 % generate triangular mesh of diameters approximating to  2000 m
 fout = fopen('geometry_400slab_4.jou','w+');
@@ -5,17 +41,20 @@ fprintf(fout,'%s\n','${Units(si)}');
 fprintf(fout,'%s\n','reset');
 % fprintf(fout,'%s\n','undo off');
 
+% setup contour files' folder, prefix and depth steps
+prefix = strcat('../../../2014SSE/CubitInput/csmooth2_'); % folder + contour files' prefix
 depth = [5 20 30 40 50 60];
 
 for dd = 1:length(depth)
-   fname = strcat('../../../2014SSE/CubitInput/csmooth2_',num2str(depth(dd)),'.txt');
+   fname = strcat(prefix,num2str(depth(dd)),'.txt');
    subgrd = load(fname);
    subgrd = subgrd(1:3:end,:);
-   dnum = find(subgrd(:,2) < 170 & subgrd(:,2)> -230);
+
+   dnum = find(subgrd(:,2) < 170.0 & subgrd(:,2)> -230.0); % if use subdomain from original contour file
+
    subgrd = subgrd(dnum,:)*1e3;
 
-%    subgrd(:,3) = -subgrd(:,3)-60e3 ;
-   dep = (depth(dd)-4.9)*1e3; % depth should in positive.
+   dep = (depth(dd))*1e3; % pay attention depth should in positive.
    
    fprintf(fout,'%s%f%s%f%s%f%s\n','create vertex x {',subgrd(1,1),'} y {',subgrd(1,2),'} z {',dep,'}');  
    fprintf(fout,'%s%d%s\n','${idPtTopeS',dd,'=Id("vertex")}');
@@ -29,67 +68,12 @@ for dd = 1:length(depth)
    fprintf(fout,'%s%d%s%d%s\n','create curve spline vertex {idPtTopeS',dd,'} to {idPtTopeN',dd,'}');
    fprintf(fout,'%s%d%s\n','curve {Id("curve")} name "curve',dd,'"');
    
-%    if dd >1
-%    fprintf(fout,'%s%d%s%d%s\n','create curve spline vertex {idPtTopeS',dd,'} {idPtTopeS',dd-1,'}'); 
-%    fprintf(fout,'%s%d%s\n','curve {Id("curve")} name "edgeS',dd,'"');
-%    fprintf(fout,'%s%d%s%d%s\n','create curve spline vertex {idPtTopeN',dd,'} {idPtTopeN',dd-1,'}');   
-%    fprintf(fout,'%s%d%s\n','curve {Id("curve")} name "edgeN',dd,'"');
-%    fprintf(fout,'%s%d%s%d%s%d%s%d\n','create surface curve curve',dd,' edgeN',dd,' curve',dd-1,' edgeS',dd);
-%    fprintf(fout,'%s%d%s\n','surface {Id("surface")} name "surf',dd,'"');
-%    end
 end
-% fprintf(fout,'%s%d %d %d %d %d %d %d %d %d %d %d\n','create surface curve ',1,2,3,4,5,6,7,8,9,10,11);
-% fprintf(fout,'%s\n','create curve spline vertex {idPtTopeN1} {idPtTopeN2} {idPtTopeN3} {idPtTopeN4} {idPtTopeN5} {idPtTopeN6} {idPtTopeN7} {idPtTopeN8} {idPtTopeN9} {idPtTopeN10}');
-% fprintf(fout,'%s\n','curve {Id("curve")} name "edgeN"');
-% fprintf(fout,'%s\n','create curve spline vertex {idPtTopeS1} {idPtTopeS2} {idPtTopeS3} {idPtTopeS4} {idPtTopeS5} {idPtTopeS6} {idPtTopeS7} {idPtTopeS8} {idPtTopeS9} {idPtTopeS10}');
-% fprintf(fout,'%s\n','curve {Id("curve")} name "edgeS"');
-% fprintf(fout,'%s%d%s\n','create curve spline vertex {idPtTopeN1} {idPtTopeN',dd,'}');
-% fprintf(fout,'%s\n','curve {Id("curve")} name "edgeN"');
-% fprintf(fout,'%s%d%s\n','create curve spline vertex {idPtTopeS',dd,'} {idPtTopeS1}');
-% fprintf(fout,'%s\n','curve {Id("curve")} name "edgeS"');
-% fprintf(fout,'%s%d%s\n','create surface curve curve1 curve',dd,' edgeN edgeS');
-% fprintf(fout,'%s\n','surface {Id("surface")} name "fault"');
-
-% fprintf(fout,'%s\n','create surface vertex 100 120 130 on surface fault');
-% fprintf(fout,'%s\n','delete vertex all');
-% fprintf(fout,'%s\n','delete curve all');
 
 %% create volume 
-% fprintf(fout,'%s\n','create vertex x -300 y -800 z 0'); 
-% fprintf(fout,'%s\n','${box1=Id("vertex")}');
-% fprintf(fout,'%s\n','create vertex x 500 y -800 z 0'); 
-% fprintf(fout,'%s\n','${box2=Id("vertex")}');
-% fprintf(fout,'%s\n','create vertex x 500 y 300 z 0');  
-% fprintf(fout,'%s\n','${box3=Id("vertex")}');
-% fprintf(fout,'%s\n','create vertex x -300 y 300 z 0');  
-% fprintf(fout,'%s\n','${box4=Id("vertex")}');
-% fprintf(fout,'%s\n','create surface vertex {box1} {box2} {box3} {box4}'); 
-% fprintf(fout,'%s\n','surface {Id("surface")} name "freesurf"');
-% 
-% fprintf(fout,'%s\n','sweep surface freesurf vector 0 0 -1 distance 200'); 
-% fprintf(fout,'%s\n','volume {Id("volume")} name "faultbody"');
-% fprintf(fout,'%s\n', 'Webcut volume faultbody sweep surface fault vector 0 0 1 Through_all');
-
-% fprintf(fout,'%s\n','create vertex x -100 y -250 z 60');  
-% fprintf(fout,'%s\n','create vertex x 300 y -250 z 60');  
-% fprintf(fout,'%s\n','create vertex x -100 y 200 z 60');  
-% fprintf(fout,'%s\n','create vertex x 300 y 200 z 60'); 
-% fprintf(fout,'%s\n','delete surface fault');
 
 fprintf(fout,'%s\n','create surface skin curve all');
 fprintf(fout,'%s\n','delete curve all');
 fprintf(fout,'%s\n','delete vertex all');
-% fprintf(fout,'%s\n','export acis "surf_top.sat" overwrite');
-
-% fprintf(fout,'%s\n','imprint all');
-% fprintf(fout,'%s\n','merge all');
-% fprintf(fout,'%s\n','rotate 45 about x');
-% fprintf(fout,'%s\n','rotate 30 about z');
-% fprintf(fout,'%s\n','save as "cascadia_mega.cub" overwrite');
-
-
-% fprintf(fout,'%s\n','${dx=0.5}');
-% fprintf(fout,'%s\n','surface fault scheme trimesh');
-% fprintf(fout,'%s\n','mesh surface fault');
 
 
