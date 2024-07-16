2a38f7d0a01c3ebadfc5f97743c5e689961675e4
Duo <d.li@gns.cri.nz>
2024-07-16 14:33:15 +1200

    update bp5

--- TriGreen/runcompile.bash
+++ TriGreen/runcompile.bash
@@ -1,1 +1,1 @@
-mpiifort sub_comdun.f mod_dtrigreen.f90 m_calc_green.f90 calc_trigreen.f90 -o calc_trigreen -g
+mpif90     sub_comdun.f mod_dtrigreen.f90 m_calc_green.f90  calc_BP5.f90 -o calc_BP5 -g
