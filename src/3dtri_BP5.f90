5e7b275472ddc90c5072d4a2672be1b47a8eea41
daisy20170101 <liduoduo07@gmail.com>
2025-08-26 22:00:22 +1200

    add copy right

--- src/3dtri_BP5.f90
+++ src/3dtri_BP5.f90
@@ -1,10 +1,41 @@
-! using varying plate convergence rate. pay attention to theta !!!!
-! yt(2*i) should not be negative!!!!
-!!! with two buffer zones both sides of 20 km wide each
-!! this is a restart code with locked seismogenic zone where slip==0
+!===============================================================================
+! Copyright (c) 2024 TriBIE Development Team
+! All rights reserved.
+!
+! This software is part of the TriBIE (Triangular Boundary Integral Element)
+! earthquake simulation package. It implements the 3D triangular boundary
+! integral equation method for dynamic rupture simulation.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice,
+!    this list of conditions and the following disclaimer.
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+! 3. Neither the name of the copyright holder nor the names of its contributors
+!    may be used to endorse or promote products derived from this software
+!    without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+! ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+! LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+! SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+! INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+! CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+! THE POSSIBILITY OF SUCH DAMAGE.
+!
+! For questions or support, please contact the TriBIE Development Team.
+!===============================================================================
+
 !------------------------------------------------
 ! D. Li
-! Last modified Aug. 2018
+! Last modified Aug. 2025
 ! 
 ! SEAS benchmark BP5
 ! May 16, 2021
