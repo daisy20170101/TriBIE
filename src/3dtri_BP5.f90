998fb901bbfde98c7b5432646b04d372481cbd33
daisy20170101 <liduoduo07@gmail.com>
2025-09-24 13:34:16 +1200

    fix error

--- src/3dtri_BP5.f90
+++ src/3dtri_BP5.f90
@@ -630,7 +630,7 @@ end if
                 write(i,'(A,1x,A,1x,A,1x,A,1x,A,1x,A,1x,A,1x)')'t','slip','slip_rate','shear_stress','pore_pressure','Darcy_vel','state'
                 write(i,100)'# Below is the time-series data.'          
         end do
- 190    format(A)
+ 100    format(A)
 end if
  
   Ifileout = 60   !file index, after 47
