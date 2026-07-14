9652aab35dc6850c9ba5f3049117120e9d08e337
daisy20170101 <liduoduo07@gmail.com>
2026-07-15 10:59:14 +1200

    add Nikkhoo

--- NikkhooWalter2015/calc_nikkhoo.f90
+++ NikkhooWalter2015/calc_nikkhoo.f90
@@ -360,8 +360,8 @@ subroutine calc_nikkhoo_allcell(myid, size, Nt, arr_vertex, arr_cell, &
   l_miu = parm_l / parm_miu
 
   ! Unit strike-slip (ss=1, ds=0, ts=0)
-  ss = 0.d0
-  ds = -1.d0
+  ss = -1.d0
+  ds = 0.d0
   ts = 0.d0
 
   ! Allocate work arrays
